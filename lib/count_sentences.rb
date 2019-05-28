f12cf55e9f5a68950221b29323274784e5b0c3ee
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-28 22:52:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,16 +3,12 @@ require 'pry'
 class String
 
   def sentence?
-    "Hi, my name is Sophie.".end_with?(".")
-    "Hi!".end_with?(".")
     self.end_with?(".") 
     
   end
 
   def question?
-    "What's your name?".end_with?("?")
-    "My name is Kateryna."end_with("?")
-    self.end_with("?")
+      self.end_with("?")
 
   end
 
