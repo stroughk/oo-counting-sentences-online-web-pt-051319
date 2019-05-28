15298690abe080745eaba7e62f37c68eba62d949
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-28 22:53:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -8,12 +8,12 @@ class String
   end
 
   def question?
-      self.end_with("?")
+      self.end_with?("?")
 
   end
 
   def exclamation?
-
+     self.end_with?("!")
   end
 
   def count_sentences
